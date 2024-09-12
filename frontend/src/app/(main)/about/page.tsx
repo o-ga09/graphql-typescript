3bc78033fadb02473b785045ed6e684d3d5fec43
taiti09 <taitiabe1997@gmail.com>
2024-09-13 07:45:52 +0900

    Refactor .gitignore and add .env file

--- frontend/src/app/(main)/about/page.tsx
+++ frontend/src/app/(main)/about/page.tsx
@@ -3,25 +3,26 @@ import { Suspense } from "react";
 import About from "@/components/about/About";
 import LayoutWithSidebar from "@/components/layout/LayoutWithSidebar";
 import Loading from "@/app/loading";
+import { microcms } from "@/lib/microcms";
+import { AboutType } from "@/types";
 // import { microcms } from "@/lib/microcms";
 
 export const revalidate = 0;
 
 // アバウトページ
 const AboutPage = async () => {
-  const dummy = "dammy";
-  //   const about = await microcms.getList<AboutType>({
-  //     endpoint: "about",
-  //     queries: {
-  //       orders: "publishedAt",
-  //       limit: 1,
-  //     },
-  //   });
+  const about = await microcms.getList<AboutType>({
+    endpoint: "about",
+    queries: {
+      orders: "publishedAt",
+      limit: 1,
+    },
+  });
 
   return (
     <Suspense fallback={<Loading />}>
       <LayoutWithSidebar>
-        <About content={dummy} />
+        <About content={about.contents[0].content} />
       </LayoutWithSidebar>
     </Suspense>
   );
--- frontend/src/app/(main)/blog/page.tsx
+++ frontend/src/app/(main)/blog/page.tsx
@@ -4,43 +4,23 @@ import { BlogType } from "@/types";
 import Blog from "@/components/blog/Blog";
 import LayoutWithSidebar from "@/components/layout/LayoutWithSidebar";
 import Loading from "@/app/loading";
+import { microcms } from "@/lib/microcms";
 
 export const revalidate = 0;
 
 // ブログページ
 const BlogPage = async () => {
-  const dummyBlog: BlogType[] = [
-    {
-      id: "dummy",
-      title: "dummy",
-      content: "dummy",
-      image: {
-        url: "/dummy.jpg",
-      },
-      category: {
-        id: "dummy",
-        name: "dummy",
-        color: "dummy",
-      },
-      ranking: 1,
-      isRecommended: true,
-      isSpecial: true,
-      createdAt: "2024-09-12",
-      publishedAt: "2024-09-12",
-      updatedAt: "2024-09-12",
+  const allBlogs = await microcms.getList<BlogType>({
+    endpoint: "blog",
+    queries: {
+      orders: "-publishedAt",
     },
-  ];
-  // const allBlogs = await microcms.getList<BlogType>({
-  //   endpoint: "blog",
-  //   queries: {
-  //     orders: "-publishedAt",
-  //   },
-  // });
+  });
 
   return (
     <Suspense fallback={<Loading />}>
       <LayoutWithSidebar>
-        <Blog blogs={dummyBlog} />
+        <Blog blogs={allBlogs.contents} />
       </LayoutWithSidebar>
     </Suspense>
   );
