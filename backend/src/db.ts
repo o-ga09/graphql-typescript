4c3940f40a1852d9f436a85d55beca432f788dc2
o-ga09 <taitiabe1997@gmail.com>
2024-11-12 23:48:06 +0900

    DB接続修正

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,15 +3,10 @@ import mysql, { createPool, PoolConnection } from 'mysql2/promise';
 export let dbconnection: mysql.Connection;
 export let Testdbconnection: PoolConnection;
 
-// Create a MySQL connection
+// Create a MySQL connection using DSN
 export const createConnection = async () => {
-	dbconnection = await mysql.createConnection({
-		host: process.env.MYSQL_HOST,
-		port: Number(process.env.MYSQL_PORT),
-		user: process.env.MYSQL_USER,
-		password: process.env.MYSQL_PASSWORD,
-		database: process.env.MYSQL_DATABASE,
-	});
+	const dsn = process.env.DATABASE_URL;
+	dbconnection = await mysql.createConnection(dsn);
 };
 
 export const getConnection = async () => {
@@ -25,14 +20,8 @@ export const closeConnection = async () => {
 };
 
 // Close the MySQL connection for Test
-export const createTestdbConnection = async (param: { host; user; port; password; dbname }) => {
-	const pool = await createPool({
-		host: param.host,
-		port: param.port,
-		user: param.user,
-		password: param.password,
-		database: param.dbname,
-	});
-
+export const createTestdbConnection = async () => {
+	const dsn = process.env.DATABASE_URL;
+	const pool = await createPool(dsn);
 	Testdbconnection = await pool.getConnection();
 };
