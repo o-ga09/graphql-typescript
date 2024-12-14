7d140e14df9c4f51dee4e9d1b6cf52dabec4e717
o-ga09 <taitiabe1997@gmail.com>
2024-12-14 18:03:16 +0900

    ESLint設定を更新し、GraphQLコード生成に必要なプラグインとドキュメントパスを追加

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -3,9 +3,14 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
   overwrite: true,
   schema: "../backend/schema.graphql",
+  documents: "lib/graphql/*.gql",
   generates: {
     "lib/generated/graphql.ts": {
-      plugins: ["typescript", "typescript-operations"],
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-apollo",
+      ],
     },
   },
 };
